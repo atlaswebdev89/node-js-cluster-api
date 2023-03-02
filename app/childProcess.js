eedaa73a08f8b870923fd38bdbdb68ea15b4d523
atlas <atlas.webdev89@gmail.com>
2023-03-02 09:58:42 +0300

    update

---
+++ app/childProcess.js
@@ -0,0 +1,17 @@
+import { spawn }  from 'node:child_process';
+const ls = spawn('cd /etc/ && ls -l');
+console.time("start");
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+  console.timeEnd("start");
+});
+
+
